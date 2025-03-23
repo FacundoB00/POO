6c3dab796ad775bffdfd6a5ebd00a5e9a8587d78
facundo <facundobaez00@gmail.com>
2025-03-23 16:04:21 -0300

    primer commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EjercicioAbstraccionEjemplo 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public class televisor{
+
+            static string marca;
+            static string estado;
+            static int pulgadas;
+
+        }
+        public void endencerTelevisor()
+        {
+            Console.WriteLine("la television se encendio");
+        }
+        public void apagarTelevisor()
+        {
+            Console.WriteLine("la television se apago");
+        }
+    }
+
+
+    }
+
+
